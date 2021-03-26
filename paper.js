e65ccb4e3598f24129b9959632b4d038b8fe2f6e
roseSanvee <75472083+roseSanvee@users.noreply.github.com>
2021-03-26 23:10:07 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,18 @@
+class Paper extends circularMatter{
+ constructor(x,y){
+    super(x,y,50,50);
+   
+ }
+
+ }
+display()
+{ 
+
+   super.display;
+   var posBottom=this.bottomBody.position;
+   var posLeft=this.leftWallBody.position;
+   var posRight=this.rightWallBody.position;
+
+   
+}
+  
